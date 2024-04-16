ec7a6893e4d0ab29bdb2c45557ca21ab69c60b10
Sneaky Mannequin <62338341+Prowderypulp@users.noreply.github.com>
2024-04-16 19:34:51 +0100

    Update Downloader.sh

--- Downloader.sh
+++ Downloader.sh
@@ -5,5 +5,8 @@ sudo pacman -Sy git cmake wget base-devel
 git clone "https://github.com/DReichLab/AdmixTools"
 cd "$HOME/AdmixTools/src"
 make clobber
+make all
 make install
-
+cd
+sudo cp -r $HOME/AdmixTools/bin/. /bin/
+echo "Hey! Your computer now have AdmixTools Capabilities. You can use qpAdm and qpWave as much as you like"
