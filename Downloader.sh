05c5fe8ec46a8fcc08bc6b4d989753afabee4f50
Sneaky Mannequin <fedchs@icloud.com>
2024-04-22 22:00:29 +0100

    Updated Downloader.sh

--- Downloader.sh
+++ Downloader.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 echo "Welcome to AdmixTools Setup \n"
 cd $HOME
-sudo pacman -Sy git cmake wget base-devel
+sudo pacman -Sy git cmake wget base-devel lgsl openblas
 git clone "https://github.com/DReichLab/AdmixTools"
 cd "$HOME/AdmixTools/src"
 make clobber
