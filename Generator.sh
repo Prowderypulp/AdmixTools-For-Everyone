9ac2fcbc68bf8285ae53787a0b4685598f7be6c9
Sneaky Mannequin <techcs@protonmail.com>
2024-06-24 12:38:33 +0100

    Update Generator.sh

--- Generator.sh
+++ Generator.sh
@@ -2,8 +2,8 @@
 # The support is currently limited to 23andme.
 cd $HOME/AdmixTools-For-Everyone/files/
 plink --23file raw.txt --make-bed --out output
-plink --bfile output --geno 0.05 --make-bed --out output_qc1
-plink --bfile output_qc1 --mind 0.05 --make-bed --out output_qc2
+plink --bfile output --geno 0.05 --make-bed --out output_1
+plink --bfile output_1 --mind 0.05 --make-bed --out output_2
 convertf -p convertf_param.par
 
 
